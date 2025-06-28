bdbc6bc7175e1e852b533e61102c8001af3ff0e9
LiamKircher <kircherliam@gmail.com>
2025-06-28 09:46:48 -0700

    adjusted template

--- templates.py
+++ templates.py
@@ -1,6 +1,6 @@
 TEMPLATES = {
     1: [
-        "Hey {Business}, quick q: do you currently work with a cleaning team? Reason I ask is because here at Penticton Janitorial we are offering 2 weeks free for a trial period to showcase our quality. Let me know if you are interested and we can set you up immediately.",
+        "Hey {Name}, quick q: do you currently work with a cleaning team? Reason I ask is because here at Penticton Janitorial we are offering 2 weeks free for a trial period to showcase our quality. Let me know if you are interested and we can set you up immediately.",
     ],
     2: [
         "Just checking in. Let me know if you have any interest in the offer."
